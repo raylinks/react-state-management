19f2b02d98283e041b0bb26228e89c8441da80b9
Raymond Rilwan <olawuyirilwan98@gmail.com>
2021-02-27 21:56:10 +0100

    add movies

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, {useState} from 'react';
 import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 import Nav from './Nav';
-import MovieProvider from './MovieContext';
+import {MovieProvider} from './MovieContext';
 
 
 function App() {
 
   return(
-  
+  <MovieProvider>
     <div className="app">
       <div className="todo-list">
         <Nav/>
+        <AddMovie />
           <MovieList  />
       </div>
     </div>
+    </MovieProvider>
   )
   
 }
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,17 +1,17 @@
 import React, {useState, useContext} from 'react';
 import Movie from './Movie';
-import {MovieContext, MovuieContext} from './MovieContext';
+import {MovieContext} from './MovieContext';
 
 const MovieList = () => {
-    const value  =useContext(MovieContext);
+    const [movies,setMovies]  = useContext(MovieContext);
 
     return (
     <div>
-        <h1> {value}</h1>
-        {/* {movies.map(bukola => (
+      
+        {movies.map(bukola => (
           <Movie  motorcar={bukola.price} key={movies.id} name={bukola.name}/>
         ))
-    } */}
+    } 
     </div> );
 }
 
