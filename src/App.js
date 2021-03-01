795af7dbe55be521587fe14a03529c05ac5cacb0
Raymond Rilwan <olawuyirilwan98@gmail.com>
2021-03-01 07:53:11 +0100

    remove unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import MovieList from './MovieList';
 import AddMovie from './AddMovie';
 import Nav from './Nav';
@@ -10,13 +10,11 @@ function App() {
   return(
   <MovieProvider>
     <div className="app">
-      <div className="todo-list">
         <Nav/>
         <AddMovie />
           <MovieList  />
-      </div>
     </div>
-    </MovieProvider>
+  </MovieProvider>
   )
   
 }
