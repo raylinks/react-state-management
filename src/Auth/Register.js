422f18a65c176c57a6916c7ea445a9e6330744cf
Raymond Rilwan <olawuyirilwan98@gmail.com>
2021-03-21 01:13:11 +0100

    registration

--- src/Auth/Register.js
+++ src/Auth/Register.js
@@ -1,13 +1,14 @@
 import React, {useContext, useRef, useState} from 'react';
  import {Form, Button, Card, Alert} from 'react-bootstrap';
  import {Link } from 'react-router-dom';
- import {signUp} from '../Context/AuthContext';
+ import {useAuth} from '../Context/AuthContext';
 
 const Register = () => {
+    const nameRef = useRef();
     const emailRef = useRef();
     const passwordRef = useRef();
    // const { currentUser} = useAuth();
-   const {signUp} = useAuth();
+ const {signUp} = useAuth();
     const passwordConfirmRef = useRef();
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
@@ -34,6 +35,10 @@ const Register = () => {
         <h2 className="text-center mb-4">Sign up</h2>
         {error && <Alert variant="danger">{error}</Alert>}
         <Form onSubmit={handleSubmit}>
+        <Form.Group id="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" ref={nameRef} required />
+        </Form.Group>
         <Form.Group id="email">
             <Form.Label>Email</Form.Label>
             <Form.Control type="email" ref={emailRef} required />
@@ -45,7 +50,7 @@ const Register = () => {
         </Form.Group>
         <Form.Group id="password-confirm">
             <Form.Label>Password Confirmation</Form.Label>
-            <Form.Control type="email" ref={passwordConfirmRef} required />
+            <Form.Control type="password" ref={passwordConfirmRef} required />
         </Form.Group>
         <Button disabled={loading} className="w-100" type="submit">Sign up</Button>
 
--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -22,10 +22,17 @@ export function AuthProvider({children}){
     //                 return false;
     //             }
     //         }
+    const user = {
+        name: '',
+        email:'',
+        password:''
+    };
 
-    async function signUp(email,password){
+    async function signUp(name,email,password){
         try{
-            const response = await axios.post(UrlService/registerUrl(), email);
+            const response = await axios.post("http://localhost:8000/api/auth/register", { name: 'mm',
+            email:'mm@gmail.com',
+            password:'11111'});
             return response.data;
         }catch (error){
             console.error('Error', error.response);
